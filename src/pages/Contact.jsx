359558552a9a9195afee3056e4908beea6c76db6
leviathan <jake66martin@hotmail.com>
2024-01-10 10:26:30 -0500

    working on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,35 @@ import '../App.css'
 
 export default function Contact() {
   return (
-    <div className="d-flex justify-content-center align-items-center text-white position-absolute bg-dark w-50 h-50 top-25 left-25">
-      <p>Good</p>
+    <div className="d-flex flex-column justify-content-center align-items-center text-white position-absolute bg-dark w-50 h-50 top-25 left-25">
+      <p>Contact</p>
+      <form className="form d-flex flex-column" onSubmit=''>
+        <input
+          value=''
+          name="name"
+          onChange=''
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value=''
+          name="email"
+          onChange=''
+          type="text"
+          placeholder="E-mail"
+        />
+        <input
+          value=''
+          name="message"
+          onChange=''
+          type="text"
+          placeholder="Message"
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
+    
   );
 }
