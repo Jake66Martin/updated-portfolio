ecf9a73308b001edddd7a69ba70ab4d624e84da3
leviathan <jake66martin@hotmail.com>
2024-01-11 12:26:12 -0500

    added all application pictures to portfolio page

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -4,7 +4,7 @@ export default function Aboutme() {
   return (
     <div className="d-flex justify-content-center align-items-center text-white position-absolute w-50 h-50 top-25 left-25">
       <div className="h-100 w-25 d-flex justify-content-center">
-        <img src="src\assets\TRY1.jpg" alt="Description of the image" />
+        <img src="src\assets\TRY1.jpg" alt="Image of myself" />
       </div>
       <div className="h-100 w-75">
         <p>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,14 +5,26 @@ export default function Portfolio() {
     <div className="d-flex justify-content-center align-items-center text-white position-absolute w-100 height">
       <div className="w-100 h-100 d-flex flex-column">
         <div className="w-100 h-50 d-flex">
-          <div className="application-images"></div>
-          <div className="application-images"></div>
-          <div className="application-images"></div>
+          <div className="application-div d-flex justify-content-center align-items-center">
+            <img src="src\assets\cravings.png" alt="Image of myself" className="img-size" />
+          </div>
+          <div className="application-div d-flex justify-content-center align-items-center">
+            <img src="src\assets\day-schedule.png" alt="Image of myself" className="img-size"/>
+          </div>
+          <div className="application-div d-flex justify-content-center align-items-center">
+            <img src="src\assets\first-portfolio.png" alt="Image of myself" className="img-size"/>
+          </div>
         </div>
         <div className="w-100 h-50 d-flex">
-          <div className="application-images"></div>
-          <div className="application-images"></div>
-          <div className="application-images"></div>
+          <div className="application-div d-flex justify-content-center align-items-center">
+            <img src="src\assets\password generator.png" alt="Image of myself" className="img-size"/>
+          </div>
+          <div className="application-div d-flex justify-content-center align-items-center">
+            <img src="src\assets\tech-blog.png" alt="Image of myself" className="img-size"/>
+          </div>
+          <div className="application-div d-flex justify-content-center align-items-center">
+            <img src="src\assets\TR-0001.png" alt="Image of myself" className="img-size"/>
+          </div>
         </div>
       </div>
     </div>
