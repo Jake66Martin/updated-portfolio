ea1e9a7ce80184408227a9d55ac3fa57dea176e7
leviathan <jake66martin@hotmail.com>
2024-01-11 11:57:07 -0500

    created flex boxes for images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,8 +3,17 @@ import "../App.css";
 export default function Portfolio() {
   return (
     <div className="d-flex justify-content-center align-items-center text-white position-absolute w-100 height">
-     
-        <div className="div-frac">
+      <div className="w-100 h-100 d-flex flex-column">
+        <div className="w-100 h-50 d-flex">
+          <div className="application-images"></div>
+          <div className="application-images"></div>
+          <div className="application-images"></div>
+        </div>
+        <div className="w-100 h-50 d-flex">
+          <div className="application-images"></div>
+          <div className="application-images"></div>
+          <div className="application-images"></div>
+        </div>
       </div>
     </div>
   );
