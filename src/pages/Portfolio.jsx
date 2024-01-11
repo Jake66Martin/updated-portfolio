93a88c55bccc3706c0732989f3c3c0a8e9f2e308
leviathan <jake66martin@hotmail.com>
2024-01-10 19:23:20 -0500

    trying to fix portfolio section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,12 @@
-import '../App.css'
+import "../App.css";
 
 export default function Portfolio() {
   return (
-    <div className="d-flex justify-content-center align-items-center text-white position-absolute bg-dark w-50 h-50 top-25 left-25">
-      <p>My</p>
+    <div className="d-flex justify-content-center align-items-center text-white position-absolute w-100 h-50">
+      {/* <p>My</p> */}
+      <div className="d-flex flex-column w-100 h-100">
+        
+      </div>
     </div>
   );
 }
