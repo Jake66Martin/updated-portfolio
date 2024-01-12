db8377ddec08ed2551fc88c952a16f15d6e3b00a
leviathan <jake66martin@hotmail.com>
2024-01-12 13:28:37 -0500

    added download link for resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ import '../App.css'
 export default function Resume() {
   return (
     <div className="d-flex justify-content-center align-items-center text-white position-absolute bg-dark w-50 h-50 top-25 left-25">
-      <p>Friend</p>
+     <a href='https://docs.google.com/document/d/1ZG0bdVRG47nXCcs6ZqHlmErJFk4SpOdqUTfMVXfknZw/export?format=pdf' download='Jacobs resume.pdf'>resume</a>
     </div>
   );
 }
