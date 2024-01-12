66b3fa6756345604b015541b00f00aa60a1e32cf
leviathan <jake66martin@hotmail.com>
2024-01-11 22:32:42 -0500

    added validations to input fields

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,5 @@
+import "../App.css";
+
 
 import { useState } from 'react';
 
@@ -9,11 +11,19 @@ export default function Contact() {
   const [message, setMessage] = useState('');
 
 
+  const handleEmpty = (e) => {
+    const { name } = e.target;
+    return (
+      name === "name" ? alert('Please enter a name') : name === "email" ? alert('Please enter an email') : alert('Please enter a message')
+    )
+    }
+
+  
 
 
   const handleInputChange = (e) => {
     const { name , value } = e.target;
-
+     console.log(e)
     return (
       name === 'name' ? setName(value) : name === 'email' ? setEmail(value) : setMessage(value)
 
@@ -32,7 +42,7 @@ export default function Contact() {
 
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center text-white position-absolute w-50 h-50 top-25 left-25">
+    <div className="d-flex flex-column justify-content-center align-items-center text-white position-absolute w-100 height">
       <p className='my-3 fs-4'>Contact</p>
       <form className="form d-flex flex-column w-100 h-100" onSubmit={handleFormSubmit}>
         <input
@@ -42,8 +52,10 @@ export default function Contact() {
           type="text"
           placeholder="Name"
           className='my-1 align-self-md-center'
+          onBlur={handleEmpty}
           required
         />
+        <p className="text-center">dafgdfg</p>
         <input
           value={email}
           name="email"
@@ -51,17 +63,21 @@ export default function Contact() {
           type="text"
           placeholder="E-mail"
           className='my-1 align-self-md-center'
+          onBlur={handleEmpty}
           required
         />
+        <p className="text-center">dfgadfg</p>
         <textarea
           value={message}
           name="message"
           onChange={handleInputChange}
           type="text"
           placeholder="Message"
-          className='my-1 h-50 w-50 align-self-center'
+          className='my-1 h-25 w-25 align-self-center'
+          onBlur={handleEmpty}
           required
         />
+        <p className="text-center">dfgadg</p>
         <button 
         type="submit" 
         className='my-1 align-self-md-center'
