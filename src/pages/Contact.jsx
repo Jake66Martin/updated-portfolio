cbf1111812475b442d6d008cd7977e1bb3ca4bc8
leviathan <jake66martin@hotmail.com>
2024-07-10 18:29:36 -0400

    making changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -62,7 +62,7 @@ export default function Contact() {
 
   return (
     <>
-      {!isMobile ? (
+      {/* {!isMobile ? (
         <div className="d-flex flex-column justify-content-center align-items-center text-white position-absolute w-100 height">
           <p className="my-3 fs-4">Contact</p>
           <form
@@ -159,7 +159,77 @@ export default function Contact() {
             <p className="text-white align-self-center">(613)501-0898</p>
           </form>
         </div>
-      )}
+      )} */}
+      <section className="bg-light py-3 py-md-5 contact-height">
+      <Container>
+        <Header />
+
+        <Row className="justify-content-lg-center">
+          <div className="col-12 col-lg-9">
+            <div className="bg-white border rounded shadow-sm overflow-hidden">
+              <Form onSubmit={formSubmit}>
+                <Row className="gy-4 gy-xl-5 p-4 p-xl-5">
+                  <div className="col-12">
+                    <label htmlFor="fullname" className="form-label">
+                      Full Name <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      value={name}
+                      type="text"
+                      className="form-control"
+                      id="fullname"
+                      name="fullname"
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="col-12 col-md-6">
+                    <label htmlFor="email" className="form-label">
+                      Email <span className="text-danger">*</span>
+                    </label>
+                    <div className="input-group">
+                      <input
+                        value={email}
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <label htmlFor="message" className="form-label">
+                      Message <span className="text-danger">*</span>
+                    </label>
+                    <textarea
+                      value={message}
+                      className="form-control"
+                      id="message"
+                      name="message"
+                      rows="3"
+                      onChange={handleChange}
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="col-12">
+                    <div className="d-grid">
+                      <button
+                        className="btn btn-danger btn-lg"
+                        type="submit"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </Row>
+              </Form>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </section>
     </>
   );
 }
